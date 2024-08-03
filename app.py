10a1e749187994122bb8b3b666702324f0c4093c
Amina Sagheer <172102325+Amina-Sagheer@users.noreply.github.com>
2024-08-03 13:43:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 # Retrieve configurations from environment variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///default.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'defaultsecretkey')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'HeadStarterHackathonProject')
 
 
 db = SQLAlchemy(app)
